e73cf69213813f18b9d774341c3d9bb3d0d125cb
Risha <153814332+Rishi-Mishima@users.noreply.github.com>
2026-02-23 17:58:22 +0800

    feat: list-twopointers-LC86 solutions added

---
+++ src/list/TwoPointers/LC86_Partition_list_solution2.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode(-1); 
+        ListNode large = new ListNode(-1);
+
+        ListNode smallHead = small; 
+        ListNode largeHead = large; 
+
+        while(head != null){
+            if(head.val < x){
+                small.next = head; 
+                small = small.next; 
+            }else{
+                large.next = head; 
+                large = large.next; 
+            }
+
+            head = head.next; 
+        }
+
+        large.next = null; 
+        small.next = largeHead.next; 
+
+        return smallHead.next; 
+
+
+    }
+}
